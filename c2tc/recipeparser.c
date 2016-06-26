f48980e1b8484c4ac7b910781addccc203993c1e
luciusmagn <luk.hozda@gmail.com>
2016-06-26 12:51:51 +0200

    Started reworking recipeparser so that it uses just an ad hoc parser rather than mpc, improved recipe comment skipping mechanism

--- c2tc/recipeparser.c
+++ c2tc/recipeparser.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#ifdef _WIN32
 #include <io.h>
+#include <direct.h>
+#else
+#include <unistd.h>
+#endif
 
 #include "mpc.h"
 #include "inttypes.h"
@@ -10,36 +16,7 @@
 
 int32 recipemain(int32 argc, char** argv)
 {
-	mpc_parser_t* spaces = mpc_new("spaces");
-	mpc_parser_t* ident = mpc_new("ident");
-	mpc_parser_t* option = mpc_new("option");
-	mpc_parser_t* type = mpc_new("type");
-	mpc_parser_t* libtype = mpc_new("libtype");
-	mpc_parser_t* file = mpc_new("file");
-	mpc_parser_t* target = mpc_new("target");
-	mpc_parser_t* recipe = mpc_new("recipe");
-
-	mpc_err_t* err = mpca_lang(MPCA_LANG_WHITESPACE_SENSITIVE,
-		//Hours wasted: 7
-		" spaces      : /([ ]|\\t)*/ ;                                                                          \n"//
-		" ident       : <spaces> /[a-zA-Z][a-zA-Z0-9_-]*/ <spaces> ;                                            \n"//
-		" option      : <spaces> /\\$/ <ident>+ /\\s/+ ;                                                        \n"//
-		" type        : \"target\" | \"lib\" ;                                                                  \n"//
-		" libtype     : \"shared\" | \"static\" ;                                                               \n"//
-		" file        : <spaces> /[a-zA-Z0-9_-\\.\\\\\\/]+/ <spaces> /\\s/+ ;                                   \n"//
-		" target      : <spaces> <type> <ident> <libtype>? /\\s/+ (<option> | <file>)+ \"end\" <spaces> ;       \n"//
-		" recipe      : /^/ <target>+ /$/;                        \n",
-		spaces, ident, option, type, libtype, file, target,
-		recipe, NULL
-		);
-	mpc_optimise(recipe);
-	if (err != NULL)
-	{
-		mpc_err_print(err);
-		mpc_err_delete(err);
-		exit(1);
-	}
-
+	char* recipepath;
 	if (argc > 1)
 	{
 		char* commentlessrecipe;
@@ -78,9 +55,6 @@ int32 recipemain(int32 argc, char** argv)
 			}
 			fclose(current);
 
-			//comment skip
-			//puts(recipetxt);
-			//puts("\n======================\n");
 			char* temp = malloc(sizeof(char) * strlen(recipetxt));
 			int8 flag = 0;
 			int32 cursor = 0;
@@ -94,6 +68,8 @@ int32 recipemain(int32 argc, char** argv)
 				else if (recipetxt[i] == '\n' && flag == 1)
 				{
 					flag = 0;
+					temp[cursor] = recipetxt[i];
+					cursor++;
 				}
 				else if (recipetxt[i] == '#' && flag == 0)
 				{
@@ -102,43 +78,49 @@ int32 recipemain(int32 argc, char** argv)
 			}
 			commentlessrecipe = malloc(sizeof(char) * strlen(temp));
 			strcpy(commentlessrecipe, temp);
-			//puts(commentlessrecipe);
 			free(recipetxt);
-		}
-		mpc_result_t r;
-		if (mpc_parse(argv[1], commentlessrecipe, recipe, &r))
-		{
-			mpc_ast_print(r.output);
-			recipe_walk(r.output, NULL);
-			mpc_ast_delete(r.output);
 		}
 		else
 		{
-			mpc_err_print(r.error);
-			puts("dump");
-			puts("=======================");
-			printf("failure:%s\nfilename:%s\n", r.error->failure, r.error->filename);
-			mpc_err_delete(r.error);
+			puts("error: recipe file can not be read");
 		}
-		//mpc_print(recipe);
 
 	}
 	else
 	{
-		mpc_result_t r;
-		if (mpc_parse_pipe("<stdin>", stdin, recipe, &r))
-		{
-			mpc_ast_print(r.output);
-			mpc_ast_delete(r.output);
-		}
-		else
-		{
-			mpc_err_print(r.error);
-			mpc_err_delete(r.error);
-		}
+		puts("error: not enough command-line arguments.");
 	}
-
-	mpc_cleanup(6, spaces, ident, option, file, target, recipe);
-
 	return 0;
+}
+
+char* findrecipe()
+{
+	char buf[1024];
+
+#ifdef _WIN32
+	_getcwd(buf, 1024);
+	while (strlen(buf) != 3)
+#else
+	getcwd(buf, 1024)
+	while (strcmp(buf, "/") != 0)
+#endif
+	{
+#ifdef _WIN32
+		if (access("recipe.txt", _A_NORMAL) == 0)
+#else
+		if (access("recipe.txt", R_OK) == 0)
+#endif
+		{
+			char path[1024];
+#ifdef _WIN32
+			_getcwd(path, 1024);
+#else
+			getcwd(buf, 1024);
+#endif
+			strcat(path, "recipe.txt");
+			return path;
+		}
+		chdir("../");
+	}
+	return -1;
 }
