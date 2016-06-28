14d9cbbc979ff63b5937a07fc24e99b6e5a46095
luciusmagn <luk.hozda@gmail.com>
2016-06-28 11:29:23 +0200

    more errors

--- c2tc/errors.c
+++ c2tc/errors.c
@@ -1,4 +1,6 @@
 #include "errors.h"
+#include "vasprintf.h"
+#include "microtest.h"
 
 void init_errors()
 {
@@ -8,4 +10,11 @@ void init_errors()
 	enoaccs = error_new(4, "ENOACCS");
 	ebadtok = error_new(5, "EBADTOK");
 	enotoks = error_new(6, "ENOTOKS");
+	eparsef = error_new(7, "EPARSEF");
+}
+
+char* get_file_error(char* filename)
+{
+	char* str; asprintf(&str, "Failed to parse file" ANSI_COLOR_GREEN " %s" ANSI_COLOR_RESET ".", filename);
+	return str;
 }
--- c2tc/errors.h
+++ c2tc/errors.h
@@ -9,7 +9,8 @@ error_t enoargs; //error no arguments
 error_t enoaccs; //error no access to file
 error_t ebadtok; //error bad token
 error_t enotoks; //error not enough tokens
+error_t eparsef; //error parsing a file failed
 
 void init_errors();
-
+char* get_file_error(char* filename);
 #endif
