6af062d9615fcc6d238131649a0109f8ea4ec0d8
luciusmagn <luk.hozda@gmail.com>
2016-07-04 14:30:56 +0200

    Fixed header and started working on first pass

--- c2tc/c2.h
+++ c2tc/c2.h
@@ -1,8 +1,9 @@
 #ifndef c2_h
 #define c2_h
 #include "mpc.h"
-#include "vector.h"
 #include "inttypes.h"
+#include "recipe.h"
+#include "vector.h"
 
 #define ALIASTYPE 0x00
 #define FUNCTYPE 0x01
@@ -30,4 +31,8 @@ typedef struct
 	char* target;
 	char* name;
 } module;
+
+void cleanup_trg(target* trg);
+void cleanup(mpc_ast_t* node);
+void deluseless(mpc_ast_t* node);
 #endif
