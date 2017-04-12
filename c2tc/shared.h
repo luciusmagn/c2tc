fdc3845396942b446134df5ed82da5669d048007
Lukáš Hozda <luk.hozda@gmail.com>
2017-04-12 10:17:41 +0200

    fix c2main() declaration

--- c2tc/shared.h
+++ c2tc/shared.h
@@ -16,7 +16,7 @@
 /// <summary>
 ///  This function is the c2 parser main entrypoint
 /// </summary>
-mpc_ast_t* c2main(int32 argc, char** argv);
+int c2main(int32 argc, char** argv);
 /// <summary>
 ///  This function parses C2 from a file name given in the argument and returns AST
 /// </summary>
