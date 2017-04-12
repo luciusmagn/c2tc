a821f9e34ebb40e9e661a786b1a54241d719d03e
Lukáš Hozda <luk.hozda@gmail.com>
2017-04-12 10:13:31 +0200

    change main.c, move rusty.txt

--- c2tc/main.c
+++ c2tc/main.c
@@ -12,45 +12,6 @@
 int32 main(int32 argc, char** argv)
 {
 	init_errors();
-	switch (argc)
-	{
-	case 1:
-
-		//recipemain(1, argv);
-		break;
-	case 2:
-		if (argv[1][1] == 't')
-		{
-			runtests();
-			return 0;
-		}
-		recipemain(argc, &argv[1]);
-		break;
-	case 3:
-		if (strlen(argv[1]) > 1)
-			switch (argv[1][1])
-			{
-			case 'c':
-				puts("c2parser     \n"
-					 "============="
-				);
-				c2main(2, &argv[1]);
-				break;
-			case 'r':
-				puts("recipe parser\n"
-					"=============="
-					);
-				recipemain(2, &argv[1]);
-				break;
-			default:
-				puts("mpctest -mode path");
-				break;
-			}
-		break;
-	default:
-		recipemain(2, argv);
-		break;
-	}
-	getchar();
+    c2main(argc, argv);
+    return 0;
 }
-
