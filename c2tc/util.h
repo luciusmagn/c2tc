79d6a19ac2870eda13cb79ca401ebfda92768d7c
luciusmagn <luk.hozda@gmail.com>
2017-04-12 14:33:42 +0200

    disable colors on windows until I figure out how to fix em for cmd.exe

--- c2tc/util.h
+++ c2tc/util.h
@@ -43,6 +43,7 @@ int asprintf(char** str, const char *fmt, ...);
 #endif
 
 //colors
+#ifndef _WIN32
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
@@ -50,7 +51,15 @@ int asprintf(char** str, const char *fmt, ...);
 #define ANSI_COLOR_MAGENTA "\x1b[35m"
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
-
+#else
+#define ANSI_COLOR_RED		""
+#define ANSI_COLOR_GREEN	""
+#define ANSI_COLOR_YELLOW	""
+#define ANSI_COLOR_BLUE		""
+#define ANSI_COLOR_MAGENTA	""
+#define ANSI_COLOR_CYAN		""
+#define ANSI_COLOR_RESET	""
+#endif
 //vector
 typedef struct
 {
