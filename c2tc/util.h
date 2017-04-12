60d0c46b9d84e17d757567a2654e91e9642692a9
Lukáš Hozda <luk.hozda@gmail.com>
2017-04-12 15:31:13 +0200

    move ansi colors to util.h

--- c2tc/util.h
+++ c2tc/util.h
@@ -1,6 +1,7 @@
 #ifndef inttypes_h
 #define inttypes_h
 
+//numbers
 #include <stdint.h>
 
 #ifdef _WIN32
@@ -30,6 +31,15 @@ char *skip_spaces(const char *str);
 int32 occurences(char* str, char c);
 char *str_replace(char *orig, char *rep, char *with);
 int8 issornull(char* test);
+
+//colors
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
 
 //vector
 typedef struct
