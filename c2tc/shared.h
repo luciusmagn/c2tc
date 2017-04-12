3f6fa932ae6c26c053e33592258d12056218aec3
Lukáš Hozda <luk.hozda@gmail.com>
2017-04-12 14:09:10 +0200

    update shared.h

--- c2tc/shared.h
+++ c2tc/shared.h
@@ -13,45 +13,21 @@
 #else
 #define PATH_SEPARATOR "\\"
 #endif
-/// <summary>
-///  This function is the c2 parser main entrypoint
-/// </summary>
+
 int c2main(int32 argc, char** argv);
-/// <summary>
-///  This function parses C2 from a file name given in the argument and returns AST
-/// </summary>
 mpc_ast_t* c2parse(char* filename);
-/// <summary>
-///  Entry point for the recipe parser
-/// </summary>
 int32 recipemain(int32 argc, char** argv);
 
-/// <summary>
-///  This function loops through parent directories until it finds the project root directory
-///  where a cwd is set and recipe.txt file is located. Returns path to recipe.txt
-/// </summary>
 char* findrecipe();
-
-/// <summary>
-///  This function parses the recipe file and builds a recipe structure from it.
-/// </summary>
 void parserecipe(char* recipetext);
-
-/// <summary>
-///  This function handles each line in recipe
-/// </summary>
 void handleline(char* line);
-
-/// <summary>
-///  This function runs all tests and prints verdict to stdout
-/// </summary>
 void runtests();
 
-#define test() do { testf(__FILE__, __LINE__); } while(0)
-static __inline void testf(char* file, int32 line)
+static __inline void _test(char* file, int32 line)
 {
 	printf("%s:%d\n", file, line);
 }
+#define test() do { _test(__FILE__, __LINE__); } while(0)
 
 static __inline void remove_element(mpc_ast_t** arr, int32 index, int32 array_length)
 {
@@ -66,6 +42,5 @@ char* commentless;
 mpc_ast_t* currenttree;
 mpc_ast_t* trees;
 vector* modules;
-
 
 #endif
