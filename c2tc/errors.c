42a6671f8db8b9a37f1ed7c81deae0c20759d7e0
Lukáš Hozda <luk.hozda@gmail.com>
2017-04-12 14:52:53 +0200

    merge throw.c/h and errors.c/h

--- c2tc/errors.c
+++ c2tc/errors.c
@@ -1,6 +1,29 @@
+#include "log.h"
 #include "errors.h"
 #include "vasprintf.h"
 #include "microtest.h"
+
+error_t error_new(int code, char *name)
+{
+    error_t err;
+    err.code = code;
+    err.name = name;
+    return err;
+}
+
+void throw (void *type, char *msg)
+{
+    error_t *err = (error_t *)type;
+    // default to `stderr` stream
+    if (NULL == THROW_FD)
+        THROW_FD = stderr;
+    if (THROW_FD != stderr)
+        fprintf(THROW_FD, "%s (%d): %s\n", err->name, err->code, msg);
+    else
+        log_error("%s (%d): %s\n", err->name, err->code, msg);
+
+    errors++;
+}
 
 void init_errors()
 {
--- c2tc/errors.h
+++ c2tc/errors.h
@@ -1,7 +1,32 @@
+// this file contains the throw library by joseph werle <joseph.werle@gmail.com> among other things
 #ifndef errors_h
 #define errors_h
 
-#include "throw.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include "util.h"
+
+#define QUOTE_X(t)#t
+#define QUOTE(t)QUOTE_X(t)
+#define ERROR_NEW_CONSTANT(c) error_new(c, QUOTE_X(#c))
+
+#ifdef __CYGWIN__
+#define error_t _error_t
+#endif
+typedef struct
+{
+	int code;
+	char *name;
+} error_t;
+
+FILE *THROW_FD;
+void throw (void *type, char *msg);
+error_t error_new(int code, char *name);
+int32 errors;
+
 
 error_t erdfail; //error read failed
 error_t emafail; //error memory alloc failed
