438db4aacffaf1cd8ed7d776a17690dac3c6f0c9
Lukáš Hozda <luk.hozda@gmail.com>
2017-04-13 21:22:06 +0200

    add options to shared.h

--- src/shared.h
+++ src/shared.h
@@ -8,7 +8,6 @@
 #include "util.h"
 
 #ifndef _WIN32
-#define __inline inline
 #define PATH_SEPARATOR "/"
 #else
 #define PATH_SEPARATOR "\\"
@@ -35,5 +34,18 @@ mpc_ast_t* currenttree;
 mpc_ast_t* trees;
 vector* modules;
 vector* wanted_targets;
+
+typedef struct
+{
+    unsigned int file_mode  :1;
+    unsigned int print_ast1 :1;
+    unsigned int print_ast2 :1;
+    unsigned int print_info :1;
+    unsigned int library    :1; //if the file-mode target is a library
+    vector* wanted_targets;
+    char* target_name; //name for file-mode target
+} options;
+
+options* opts;
 
 #endif
