c737f0a1aa0919e49391653d5e99ff26f2e4bd58
Lukáš Hozda <luk.hozda@gmail.com>
2017-04-13 22:42:35 +0200

    allow some basic tests with parsing

--- src/recipe.c
+++ src/recipe.c
@@ -22,6 +22,9 @@ void processtarget(target* trg)
 {
     trg->nodes = malloc(sizeof(vector));
     vector_init(trg->nodes);
+    int success = 0;
+    if(opts->test)
+        printf(ANSI_YELLOW "target: " ANSI_GREEN "%s " ANSI_RESET "\n", trg->name);
     for(int32 i = 0; i < vector_total(trg->files); i++)
     {
         mpc_ast_t* ast = malloc(sizeof(mpc_ast_t*));
@@ -34,13 +37,23 @@ void processtarget(target* trg)
                 printf("module name: %s\n", MODULE(ast)->contents);
             if(opts->print_ast1)
                 mpc_ast_print(ast);
+            if(opts->test)
+                printf(ANSI_MAGENTA "\tfile:" ANSI_CYAN " %s " ANSI_GREEN "[SUCCESS]" ANSI_RESET "\n",
+                      (char*)vector_get(trg->files, i));
+            success++;
         }
-        else
+        else if(!opts->test)
         {
             throw(&eparsef, get_file_error(vector_get(trg->files, i)));
-            log_info("compilation halted, exiting");
+            log_info("compilation failed, exiting");
             exit(-1);
         }
+        else
+            printf(ANSI_MAGENTA "\tfile:" ANSI_CYAN " %s " ANSI_RED "[FAILURE]" ANSI_RESET "\n",
+                  (char*)vector_get(trg->files, i));
     }
+    if(opts->test)
+        printf(ANSI_GREEN "TOTAL: " ANSI_YELLOW "%d/%d files parsed successfully" ANSI_RESET "\n",
+               success, vector_total(trg->files));
     //cleanup_trg(trg);
 }
