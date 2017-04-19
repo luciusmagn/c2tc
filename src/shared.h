d94323cc5fc07673bedb024966479ba9dc4c1e29
Lukáš Hozda <luk.hozda@gmail.com>
2017-04-19 07:42:33 +0200

    change names, remove unneeded global variables

--- src/shared.h
+++ src/shared.h
@@ -15,9 +15,9 @@
 
 mpc_ast_t* c2parse(char* filename);
 mpc_ast_t* ex_c2parse(char* filename);
-int32 recipemain(int32 argc, char** argv);
+void init_recipe(int32 argc, char** argv);
 
-char* findrecipe();
+char* find_recipe();
 void parserecipe(char* recipetext);
 void handleline(char* line);
 void runtests();
@@ -25,9 +25,6 @@ void runtests();
 #define test() do { printf("%s:%d\n", __FILE__, __LINE__); } while(0)
 
 int32 lsize;
-FILE* current;
-char* currenttxt;
-char* commentless;
 mpc_ast_t* currenttree;
 mpc_ast_t* trees;
 vector* modules;
