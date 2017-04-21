552269e32e0f2b568f81dfb7525effed19b7d9e8
Lukáš Hozda <luk.hozda@gmail.com>
2017-04-21 12:27:23 +0200

    add symbol_types.h

---
+++ src/symbol_types.h
@@ -0,0 +1,22 @@
+#ifndef SYMBOL_TYPES_H
+#define SYMBOL_TYPES_H
+
+typedef struct
+{
+    char* name;
+    vector* imports; //import*
+    vector* symbols; //symbol*
+    vector* trees;   //mpc_ast_t*
+} module;
+
+typedef struct
+{
+    uint8 found :1;
+    uint8 local :1;
+    uint8 with_alias :1;
+    char* alias;
+    char* ident;
+    module* module;
+} import;
+
+#endif
