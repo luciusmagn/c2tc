97cbb516ce72aa1c3d95973907254de70f4fc353
Lukáš Hozda <luk.hozda@gmail.com>
2017-06-10 11:56:14 +0200

    add build script

--- build.rs
+++ build.rs
@@ -3,12 +3,12 @@ extern crate gcc;
 fn main()
 {
 	gcc::Config::new()
-		.file("src/mpc.c")
-		.file("src/main.c")
-		.file("src/parsers.c")
-		.file("src/recipe.c")
-		.file("src/tree_transform.c")
-		.file("src/util.c")
+		.file("src/c/mpc.c")
+		.file("src/c/main.c")
+		.file("src/c/parsers.c")
+		.file("src/c/recipe.c")
+		.file("src/c/tree_transform.c")
+		.file("src/c/util.c")
 		.flag("-std=gnu99")
 		.flag("-Wno-discarded-qualifiers")
 		.compile("libc2tc.a");
